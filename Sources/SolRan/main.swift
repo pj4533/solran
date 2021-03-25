5823a233aebc16d2e079337951d5f8abebb34ab8
PJ Gray <pj@evertrue.com>
2021-03-25 11:20:53 -0400

    Serialization

--- Sources/SolRan/main.swift
+++ Sources/SolRan/main.swift
@@ -14,11 +14,23 @@ struct SolRan: ParsableCommand {
 	func run() {
         let url = URL(fileURLWithPath: filename)
         do {
+            // Reading in dungeon file
             let data = try Data(contentsOf: url)
             let decoder = JSONDecoder()
-            let dungeon = try decoder.decode(Dungeon.self, from: data)
+            var dungeon = try decoder.decode(Dungeon.self, from: data)
 
-            print("Hello dungeon: \(dungeon.title ?? "unknown title")")
+            // Modify the dungeon monsters here
+            dungeon.title = "Modified title name"
+            
+            // Serializing dungeon file out
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            if let dungeonJSONData = try? encoder.encode(dungeon) {
+                let output = String(data: dungeonJSONData, encoding: .utf8)
+                print(output ?? "<error>")
+            } else {
+                print("Error processing dungeon file")
+            }
         } catch {
             print("Error processing dungeon file")
         }
